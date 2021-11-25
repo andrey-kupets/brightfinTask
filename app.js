96129b2fb23902f9088cfcd5d24485175ae38f15
andrey-kupets <andrey.kupets@i.ua>
2021-11-25 14:53:31 +0300

    combine file + stream

--- app.js
+++ app.js
@@ -1,45 +1,94 @@
-// const csv = require('csvtojson');
 const csv = require('csvtojson');
+// const csv = require('csv');
 const StreamZip = require('node-stream-zip');
 
 const fs = require('fs');
 const path = require('path');
 
 const zipPath = path.join(process.cwd(), 'Test data.zip');
-// const qwe = path.join(__dirname, 'qwe')
-// fs.readdir(zipPath, (err, files) => {
-//   console.log(files)
-// })
-// fs.readdir(qwe, (err, files) => {
-//   console.log(files)
-// })
+
+const dateConverter = (date) => date
+  .split('/')
+  .reverse()
+  .map((item) => (item.length <= 1 ? `0${item}` : item))
+  .join('-');
+
+const dataBuilder = (item) => {
+  const {
+    name, phone, first_name, last_name, cc, amount, date
+  } = item;
+
+  return {
+    name,
+    phone,
+    person: {
+      firstName: first_name,
+      lastName: last_name
+    },
+    amount,
+    date: dateConverter(date),
+    costCenterNum: cc.slice(3)
+  };
+};
+
+const csvConfig = {
+  delimiter: '||',
+  toArrayString: true,
+  // downstreamFormat: 'array',
+  colParser: {
+    amount: 'number',
+    date: 'Date'
+  }
+};
 
 const zip = new StreamZip({
   file: zipPath,
   storeEntries: true
 });
 
-
 zip.on('ready', async () => {
-  // Take a look at the files
-
   for (const entry of Object.values(zip.entries())) {
-    // const desc = entry.isDirectory ? 'directory' :`${entry.size} bytes`;
-    // console.log(`Entry ${entry.name}: ${desc}`);
-
     // Read a file in memory
     const fileInZip = path.join(entry.name)
     const fileContent = zip.entryDataSync(fileInZip).toString('utf8');
 
-    // const fileOutZipPath = fs.writeFile(path.join(process.cwd(), 'json', 'users.js'), fileContent, err => {
-    //   console.log(err);
-    // });
-    console.log(`The content of ${fileInZip} is:\n` + fileContent);
-    //
-    // const jsonArray = await csv().fromFile(fileOutZipPath)
-    // console.log(jsonArray)
-  }
+    const outputDirPath = path.join(process.cwd(), 'outputData');
+    if (!fs.existsSync(outputDirPath)) {
+      fs.mkdir(outputDirPath, err => {
+        console.log(err);
+      });
+
+      const csvDataPath = path.join(outputDirPath, 'users.csv');
+      fs.appendFile(csvDataPath, fileContent, err => {
+        console.log(err);
+      });
+
+      const readStream = fs.createReadStream(csvDataPath);
+      const writeStream = fs.createWriteStream(path.join(outputDirPath, 'users.json'));
+
+      writeStream.write('[')
+      readStream.pipe(csv(csvConfig))
+        .once('data', () => writeStream.write('['))
+        .on('data', (chunk) => {
+          // if (chunk.length <= 2) {
+          //   writeStream.write(chunk);
+          //   return;
+          // }
+
+          const itemData = JSON.parse(chunk.toString());
+
+          const jsonItemData = dataBuilder(itemData);
+
+          writeStream.write(`${JSON.stringify(jsonItemData)},`);
+        })
+        .on('end', () => {
+          writeStream.write(']');
+          console.log('Finish');
+        }).then(() => {});
+    };
+  };
 
   // Do not forget to close the file once you're done
   zip.close();
 });
+
